56b0bc728a7407ef66304d256d8d82a9705fde27
KevinChiangMyFeel <splash.digilab@gmail.com>
2026-02-25 00:21:04 +0800

    update

--- app/data/backgrounds.ts
+++ app/data/backgrounds.ts
@@ -10,8 +10,12 @@ export interface BackgroundImage {
 export const BACKGROUND_IMAGES: BackgroundImage[] = [
   { id: 'bg-1', url: '/svg/bg/bg-1.svg' },
   { id: 'bg-2', url: '/svg/bg/bg-2.svg' },
-  { id: 'bg-3', url: '/svg/bg/bg-3.webp' },
   { id: 'bg-4', url: '/svg/bg/bg-4.webp' },
+  { id: 'bg-3', url: '/svg/bg/bg-3.webp' },
+  { id: 'bg-5', url: '/svg/bg/bg-5.webp' },
+  { id: 'bg-6', url: '/svg/bg/bg-6.webp' },
+  { id: 'bg-7', url: '/svg/bg/bg-7.webp' },
+  { id: 'bg-8', url: '/svg/bg/bg-8.webp' },
 ]
 
 /**
