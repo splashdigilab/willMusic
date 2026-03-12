13d0fcff5a43804689f6088c6f78340401507ef7
KevinChiangMyFeel <splash.digilab@gmail.com>
2026-03-12 20:42:55 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'WillMusic Sky Memo',
+      title: 'WillMusic - Post Board',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
